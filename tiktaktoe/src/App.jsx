7a119ce2a40d6eb818e2e8dd286e2ff88bc34340
SirRango <sturdivantdevon78@gmail.com>
2024-08-06 14:52:41 -0500

    few changes very buggy game(fix it)

--- tiktaktoe/src/App.jsx
+++ tiktaktoe/src/App.jsx
@@ -1,17 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 
-function Sqaure() {
-  //use state for the squares
-  const [value, setValue] = useState(null);
-  //function for when the square is clicked
-  function handleClick() {
-    console.log(value, "clicked");
-    setValue("X");
-  }
+function Sqaure({ value, onSquareClick }) {
   //rendering the squares
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -26,22 +19,38 @@ function App() {
 }
 //the board that holds all of the squares
 export function Board() {
+  const [xisNext, setXIsNext] = useState(true);
+  //moved the state to the parent
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    if (xisNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    nextSquares[i] = "X";
+    setXIsNext(!xisNext);
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Sqaure />
-        <Sqaure />
-        <Sqaure />
+        <Sqaure value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Sqaure value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Sqaure value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Sqaure />
-        <Sqaure />
-        <Sqaure />
+        <Sqaure value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Sqaure value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Sqaure value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Sqaure />
-        <Sqaure />
-        <Sqaure />
+        <Sqaure value={squares[5]} onSquareClick={() => handleClick(6)} />
+        <Sqaure value={squares[6]} onSquareClick={() => handleClick(7)} />
+        <Sqaure value={squares[7]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
